cb2742e139ada51d296c35e937a02236543f454a
RAHUL VR <iamrahulvr29@gmail.com>
2024-05-06 21:18:15 +0530

    init respo

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Header } from './components'
+
+
+
+function App() {
+
+  return (
+    <>
+      <Header/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <>
+      <div>
+        <div>
+            <img src={} alt="" />
+        </div>
+        <div>
+
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Header
